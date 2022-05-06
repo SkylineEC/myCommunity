2a330de9edbff8fe492d477010ce8c74ca454f0e
Wang Jiawen <e0724539@u.nus.edu>
2022-05-07 01:32:29 +0800

    Initial commit

---
+++ src/main/java/com/jiawen/community/controller/AlphaController.java
@@ -0,0 +1,17 @@
+package com.jiawen.community.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/Alpha")
+public class AlphaController {
+
+    @RequestMapping("/hello")
+    @ResponseBody
+    public String hello(){
+        return "Hello";
+    }
+}
---
+++ src/test/java/com/jiawen/community/CommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jiawen.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
