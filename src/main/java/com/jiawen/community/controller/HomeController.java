4cb6a7309baf78561d121094581788ba9991de2c
Wang Jiawen <e0724539@u.nus.edu>
2022-05-11 02:36:04 +0800

    add:实现分页显示功能

--- src/main/java/com/jiawen/community/controller/HomeController.java
+++ src/main/java/com/jiawen/community/controller/HomeController.java
@@ -2,6 +2,7 @@ package com.jiawen.community.controller;
 
 
 import com.jiawen.community.entity.DiscussPost;
+import com.jiawen.community.entity.Page;
 import com.jiawen.community.entity.User;
 import com.jiawen.community.service.DiscussPostService;
 import com.jiawen.community.service.UserService;
@@ -20,7 +21,6 @@ import java.util.Map;
 
 @Controller
 public class HomeController {
-
     @Autowired
     DiscussPostService discussPostService;
 
@@ -28,10 +28,18 @@ public class HomeController {
     UserService userService;
 
 
-    @RequestMapping(path = "/index", method = RequestMethod.GET)
-    public String getIndexPage(Model model){
 
-        List<DiscussPost> list =  discussPostService.findDiscussPosts(0,0,10);
+    @RequestMapping(path = "/index", method = RequestMethod.GET)
+    public String getIndexPage(Model model, Page page) {
+        //System.out.println("Page对象作为参数传入时，page.getCurrent() = " + page.getCurrent());
+        //当传入路径参数current的时候  由于函数规定有两个参数SpringBoot会自动查找第二个参数page 里面有没有current的参数
+        //如果有就会自动赋值给page.current
+        //page设置总行数
+        page.setRows(discussPostService.findDiscussPostRows(0));
+        page.setPath("/index");
+
+
+        List<DiscussPost> list =  discussPostService.findDiscussPosts(0,page.getOffset(),page.getLimit());
 
         List<Map<String, Object>> discussPosts = new ArrayList<>();
 
@@ -46,6 +54,10 @@ public class HomeController {
         }
         model.addAttribute("discussPosts",discussPosts);
 
+        //这一步可以省略 在SpringMVC 方法参数可以被初始化 会自动注入Page对象给model
+        //实际上可以通过Model获得Page
+        //model.addAttribute("page",page);
+        //在thymeleaf里面可以直接通过model访问page对象
         return "index";
 
     }
